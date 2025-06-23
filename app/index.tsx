0eb7caf0a09c70a8efd53cca7454b1ac2f7f495e
pmrna <prmna.code@gmail.com>
2025-06-24 00:45:30 +0800

    chore: added colors

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,10 @@ import ScreenWrapper from "@/components/ScreenWrapper";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const title = "Science, Technology, and Society";
+
   return (
-    <ScreenWrapper showAppBar appBarTitle="Index">
+    <ScreenWrapper showAppBar appBarTitle={title}>
       <View
         style={{
           flex: 1,
--- styles/colors.ts
+++ styles/colors.ts
@@ -1,16 +1,27 @@
 export const colors = {
   // Core system colors
-  primary: '#003B41',
-  secondary: '#45AEE0',
-  accent: '#A8772B',
-  
-  // Text and contrast
-  lightText: '#FFFFFF', 
-  darkText: '#333333',
-  
-  surface: '#ffffff',
-  elevatedSurface: '#f2f2f2',
+  primary: "#A60212",
+  secondary: "#45AEE0",
+  accent: "#A8772B",
 
-  error: '#FF5725',
-  warning: '#FFAC50',
+  // Background Color
+  surface: "#ffffff",
+
+  // Card colors
+  cardDefault: "#EFEFEF",
+  cardRecall: "#C5F9FF",
+  cardRemember: "#E4DBFF",
+  cardFacts: "#FFFF9D",
+  cardCorrect: "#9DFFC6",
+  cardActive: "#D32335",
+
+  // Shadows
+  shadow: "#151515",
+
+  // Text and contrast
+  lightText: "#FFFFFF",
+  darkText: "#333333",
+
+  error: "#FF5725",
+  warning: "#FFAC50",
 };
