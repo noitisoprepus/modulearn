c75f81093a1f3cee461aa04d15ef8919af6de59a
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-06-24 00:01:43 +0800

    Create colors class file

---
+++ styles/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  // Core system colors
+  primary: '#003B41',
+  secondary: '#45AEE0',
+  accent: '#A8772B',
+  
+  // Text and contrast
+  lightText: '#FFFFFF', 
+  darkText: '#333333',
+  
+  surface: '#ffffff',
+  elevatedSurface: '#f2f2f2',
+
+  error: '#FF5725',
+  warning: '#FFAC50',
+};
