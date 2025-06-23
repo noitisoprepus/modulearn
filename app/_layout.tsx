c5b77d5ad307c6b9521ed64cd38b214f49b416dd
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-06-24 02:54:06 +0800

    chore: add splash screen and initialize custom fonts in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'GermaniaOne': require('@/assets/fonts/GermaniaOne-Regular.ttf'),
+    'KantumruyProRegular': require('@/assets/fonts/KantumruyPro-Regular.ttf'),
+    'KantumruyProBold': require('@/assets/fonts/KantumruyPro-Bold.ttf'),
+    'KantumruyProMedium': require('@/assets/fonts/KantumruyPro-Medium.ttf'),
+    'KantumruyProLightItalic': require('@/assets/fonts/KantumruyPro-LightItalic.ttf'),
+  });
+  
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
