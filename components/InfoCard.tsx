279a230e44063ba274f14b1316f343f40c2f67c0
pmrna <prmna.code@gmail.com>
2025-06-25 18:38:09 +0800

    feat: new cards

---
+++ components/InfoCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function InfoCard() {
+  return (
+    <View>
+      <Text>Info Card</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ components/interactive/RecallCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function RecallCard() {
+  return (
+    <View>
+      <Text>Recall Card</Text>
+    </View>
+  );
+}
