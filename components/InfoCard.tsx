25be0ead7f3028c14dbbbf8d2ae92c96d451ccee
pmrna <prmna.code@gmail.com>
2025-06-27 22:35:31 +0800

    feat: InfoCard component

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,12 +1,59 @@
+import { colors } from "@/styles/colors";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import DynamicText from "./DynamicText";
+import Spacer from "./Spacer";
 
-export default function InfoCard() {
+type InfoCardVariant = "fact" | "remember";
+
+type InfoCardProps = {
+  variant: InfoCardVariant;
+};
+
+export default function InfoCard({ variant = "fact" }: InfoCardProps) {
   return (
-    <View>
-      <Text>Info Card</Text>
-    </View>
+    <>
+      {variant === "fact" ? (
+        <View style={[styles.card, { backgroundColor: colors.cardFacts }]}>
+          <DynamicText variant="header" style={{ fontSize: 16 }}>
+            Did you know?
+          </DynamicText>
+          <Spacer size={10} />
+          <DynamicText variant="paragraph" style={{ fontSize: 16 }}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
+            voluptate dolores temporibus! Sed eos nostrum dolores sequi
+            asperiores a adipisci aut, porro neque dicta, molestiae praesentium!
+            Non possimus magnam quisquam?
+          </DynamicText>
+        </View>
+      ) : (
+        <View style={[styles.card, { backgroundColor: colors.cardRemember }]}>
+          <DynamicText variant="header" style={{ fontSize: 16 }}>
+            Remember:
+          </DynamicText>
+          <Spacer size={10} />
+          <DynamicText variant="paragraph" style={{ fontSize: 16 }}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
+            voluptate dolores temporibus! Sed eos nostrum dolores sequi
+            asperiores a adipisci aut, porro neque dicta, molestiae praesentium!
+            Non possimus magnam quisquam?
+          </DynamicText>
+        </View>
+      )}
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    margin: "auto",
+    borderRadius: 10,
+    elevation: 6,
+    shadowColor: colors.shadow,
+    width: 330,
+    height: "auto",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    wordWrap: "wrap",
+  },
+});
