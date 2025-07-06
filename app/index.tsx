6010e9d5c1c0eaada3ab9c00501efd0f4245c66b
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-07-06 13:07:29 +0800

    refactor: use scrollable prop of ScreenWrapper to handle scrolling in the index screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,37 +2,35 @@ import PressableCard from "@/components/interactive/PressableCard";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import { media, modules } from "@/data/modulesContentMap";
 import { router } from "expo-router";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   const title = "Science, Technology, and Society";
 
   return (
-    <ScreenWrapper showAppBar appBarTitle={title}>
-      <ScrollView>
-        <View style={styles.layout}>
-          {modules.map((module, index) => {
-            const image =
-              module.imgSrc !== "null"
-                ? media[module.imgSrc]
-                : require("@/assets/images/react-logo.png"); // fallback
-            return (
-              <PressableCard
-                key={module.id}
-                imgSource={image}
-                module={`Module ${index + 1}`}
-                title={module.title}
-                onPress={() =>
-                  router.navigate({
-                    pathname: "/module/[id]",
-                    params: { id: module.id },
-                  })
-                }
-              />
-            );
-          })}
-        </View>
-      </ScrollView>
+    <ScreenWrapper scrollable showAppBar appBarTitle={title}>
+      <View style={styles.layout}>
+        {modules.map((module, index) => {
+          const image =
+            module.imgSrc !== "null"
+              ? media[module.imgSrc]
+              : require("@/assets/images/react-logo.png"); // fallback
+          return (
+            <PressableCard
+              key={module.id}
+              imgSource={image}
+              module={`Module ${index + 1}`}
+              title={module.title}
+              onPress={() =>
+                router.navigate({
+                  pathname: "/module/[id]",
+                  params: { id: module.id },
+                })
+              }
+            />
+          );
+        })}
+      </View>
     </ScreenWrapper>
   );
 }
