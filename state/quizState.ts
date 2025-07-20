7d3457d74fe30eff168f17cf2f9a4d8e59f9fb0e
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-07-20 23:46:24 +0800

    feat: create quiz state store

---
+++ state/quizState.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type QuizState = {
+  answers: string[];
+  setAnswers: (answers: string[]) => void;
+  clearAnswers: () => void;
+};
+
+export const useQuizState = create<QuizState>((set) => ({
+  answers: [],
+  setAnswers: (answers) => set({ answers }),
+  clearAnswers: () => set({ answers: [] }),
+}));
