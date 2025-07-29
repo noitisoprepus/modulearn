de304470513000483af8ac0da011989c4e83d980
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-07-29 21:55:02 +0800

    chore: adjust indentation in speechStore

--- store/speechStore.ts
+++ store/speechStore.ts
@@ -1,15 +1,15 @@
 import { create } from 'zustand';
 
 type SpeechState = {
-    isSpeaking: boolean;
-    isRecognizing: boolean;
-    setIsSpeaking: (speaking: boolean) => void;
-    setIsRecognizing: (recognizing: boolean) => void;
+  isSpeaking: boolean;
+  isRecognizing: boolean;
+  setIsSpeaking: (speaking: boolean) => void;
+  setIsRecognizing: (recognizing: boolean) => void;
 };
 
 export const useSpeechStore = create<SpeechState>((set) => ({
-    isSpeaking: false,
-    isRecognizing: false,
-    setIsSpeaking: (speaking) => set({ isSpeaking: speaking }),
-    setIsRecognizing: (recognizing) => set({ isRecognizing: recognizing }),
+  isSpeaking: false,
+  isRecognizing: false,
+  setIsSpeaking: (speaking) => set({ isSpeaking: speaking }),
+  setIsRecognizing: (recognizing) => set({ isRecognizing: recognizing }),
 }));
