6c270580a971b0281a49a0690ee4bbd5468c31c2
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-07-31 00:39:53 +0800

    refactor: improve voice commands in the home screen

--- app/index.tsx
+++ app/index.tsx
@@ -3,17 +3,25 @@ import ScreenWrapper from "@/components/ScreenWrapper";
 import Spacer from "@/components/Spacer";
 import { media, modules, title } from "@/data/modulesContentMap";
 import { useVoiceCommands } from "@/hooks/useVoiceCommands";
+import { useAppStore } from "@/store/appStore";
 import { useModuleStore } from "@/store/moduleStore";
+import { useSpeechStore } from "@/store/speechStore";
 import { numberWordsMap } from "@/utils/speechUtils";
 import { router } from "expo-router";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   const { setModuleIndex } = useModuleStore();
+  const {
+      setShouldRecognize,
+      setVoiceCommands,
+      setVoicePrompt,
+      setCommandCallback,
+    } = useSpeechStore();
 
-  const { promptMessage, moduleCommands } = useMemo(() => {
-    let prompt = "Welcome to ModuLearn. Say the number of the module you want to open.";
+  const { modulePrompt, moduleCommands } = useMemo(() => {
+    let prompt = "Say the number of the module you want to open.";
     const commands: string[] = [];
 
     modules.forEach((mod, i) => {
@@ -26,18 +34,44 @@ export default function Index() {
       }
     });
 
-    return { promptMessage: prompt, moduleCommands: commands };
+    return { modulePrompt: prompt, moduleCommands: commands };
   }, []);
 
-  useVoiceCommands({
-    commands: moduleCommands,
-    onCommand: (command) => {
-      const index = parseInt(numberWordsMap[command]) - 1;
-      setModuleIndex(index);
-      router.navigate("/module/[id]")
-    },
-    promptMessage: promptMessage,
-  });
+  const handleVoiceCommand = (command: string) => {
+    if (command === "modules") {
+      setVoicePrompt(modulePrompt);
+      setVoiceCommands(moduleCommands);
+      setCommandCallback((command: string) => {
+        const index = parseInt(numberWordsMap[command]) - 1;
+        setModuleIndex(index);
+        router.navigate("/module/[id]");
+      });
+      setShouldRecognize(true);
+    } else if (command === "help") {
+      // TODO: Add HELP message
+    } else {
+      console.error(`The command, "${command}", is unrecognizable.`);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    const { hasAppStarted, markAppStarted } = useAppStore.getState();
+    
+    const promptIntro = hasAppStarted
+        ? "You are now back in the home screen."
+        : "Welcome to ModuLearn.";
+
+    setVoicePrompt(`${promptIntro} Say "modules" to navigate to a module; or say "help" if you need help on how to use the app.`);
+    setVoiceCommands(["modules", "help"]);
+    setCommandCallback(handleVoiceCommand);
+    setShouldRecognize(true);
+
+    if (!hasAppStarted) {
+      markAppStarted();
+  }
+  }, []);
+  useVoiceCommands();
 
   return (
     <ScreenWrapper showAppBar appBarTitle={title}>
