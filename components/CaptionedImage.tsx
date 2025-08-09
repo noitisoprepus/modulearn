bb6807d49c7435985ec3d5f1774e8ea5b04d4f62
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2025-08-09 09:15:31 +0800

    improve: UI responsiveness of CaptionedImage component

--- components/CaptionedImage.tsx
+++ components/CaptionedImage.tsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, ImageSourcePropType, StyleSheet, View } from "react-native";
 import DynamicText from "./DynamicText";
 
 type CaptionImageProps = {
   imgSrc: ImageSourcePropType;
-  caption: string;
+  caption?: string;
 };
 
 export default function CaptionedImage({ imgSrc, caption }: CaptionImageProps) {
+  const [ratio, setRatio] = useState(1);
+
+  useEffect(() => {
+    const { width, height } = Image.resolveAssetSource(imgSrc);
+    setRatio(width / height);
+  }, [imgSrc]);
+
   return (
     <View style={styles.layout}>
-      <Image style={styles.image} source={imgSrc} />
-      <DynamicText variant="caption" style={styles.caption}>
-        {caption}
-      </DynamicText>
+      <Image
+        style={[styles.image, { aspectRatio: ratio }]}
+        source={imgSrc}
+        resizeMode="contain"
+      />
+      {caption && (
+        <DynamicText variant="caption" style={styles.caption}>
+          {caption}
+        </DynamicText>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   layout: {
-    margin: "auto",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
   },
-  image: { width: 250, height: 175, objectFit: "cover" },
-  caption: { margin: "auto" },
+  image: {
+    width: "80%",
+    height: undefined,
+  },
+  caption: {
+    marginTop: 10,
+  },
 });
